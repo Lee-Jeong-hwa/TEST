98f0bed1fbd6773352c9b8e8f144383bb43bfbd0
Lee-Jeong-hwa <jhwa0924@gmail.com>
2022-03-17 15:11:49 +0900

    Commit Test

---
+++ test/com/company/MainTest.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    public void roof1(){
+        for (int i=0;i<10;i++){
+            System.out.println(i);
+        }
+    }
+    @Test
+    public void roof2(){
+        for (int i=0;i<5;i++){
+            System.out.println(i);
+        }
+    }
+}
